a2c45848d9d94bbef878cfa10d0ce80b768ef4f6
Avinashjha111 <138882761+Avinashjha111@users.noreply.github.com>
2026-07-02 15:07:01 +0530

    new era

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] } };
+const nextConfig = { 
+  images: { 
+    remotePatterns: [{ protocol: 'https', hostname: '**' }] 
+  },
+  output: 'standalone' // 👈 Bas ye ek line niche jod di hai loop rokne ke liye
+};
+
 export default nextConfig;
