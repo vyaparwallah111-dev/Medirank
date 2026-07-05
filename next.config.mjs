64b5d31ba503c2fb419a12973e6c8680134ab9e8
Avinashjha111 <138882761+Avinashjha111@users.noreply.github.com>
2026-07-05 11:05:38 +0530

    new era

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   images: { 
     remotePatterns: [{ protocol: 'https', hostname: '**' }] 
   },
-  output: 'standalone', // 👈 Chhota bundle banane ke liye
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   async headers() {
